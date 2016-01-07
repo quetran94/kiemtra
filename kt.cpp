4b5182a50d647d324405412b82dcf272cc19aff6
duphong <duphong015@gmail.com>
2016-01-07 07:33:40 +0700

    Chuong trinh dung de kiem tra

---
+++ kt.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	int a[30], n=0, x;
+	while(n<=0 || n>=30);
+	{
+		cout<<"Nhap n: ";
+		cin>>n;
+	}
+	srand(time(NULL));
+	for(int i=0; i<n;i++)
+	{
+		a[i]=rand()%10;
+		cout<<a[i]<<" ";
+	}
+	cout<<"\nNhap gia tri muon tim: ";
+	cin>>x;
+	
+	for(int i=0; i<n;i++)
+		if(a[i]==x)
+			cout<<"Tim thay x tai vi tri: "<<i<<" "<<endl;
+}
+
